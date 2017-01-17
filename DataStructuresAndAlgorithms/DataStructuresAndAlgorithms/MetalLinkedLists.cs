41a157915525f1aa5bce73933e26ddc53b2cbd99
metaldetector <xmetaldetectorx-420@yahoo.com>
2017-01-17 15:12:11 -0800

    Started a custom generic Linked List

---
+++ DataStructuresAndAlgorithms/DataStructuresAndAlgorithms/MetalLinkedLists.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms
+{
+    public class MetalLinkedList<T>
+    {
+        Node<T> head;
+        public MetalLinkedList()
+        { }            
+        public MetalLinkedList(T val)
+        {
+            head = new Node<T>(val);
+        }
+
+        public void AddToFront(T val)
+        {
+            if(head == null)
+            {
+                head = new Node<T>(val);
+                return;
+            }
+            Node<T> newNode = new Node<T>(val);
+            newNode.next = head;
+            head = newNode;   
+        }
+
+        public void PrintAll()
+        {
+            Node<T> curr = head;
+            do
+            {
+                Console.WriteLine(curr.data);
+                curr = curr.next;
+            } while (curr != null);
+        }
+    }
+
+    class Node<T>
+    {
+        public T data;
+        public Node<T> next;
+        public Node(T val)
+        {
+            data = val;
+            next = null;
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/DataStructuresAndAlgorithms/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MetalLinkedList<string> ll = new MetalLinkedList<string>();
+            ll.AddToFront("test1");
+            ll.AddToFront("test22");
+            ll.PrintAll();
+        }
+    }
+}
